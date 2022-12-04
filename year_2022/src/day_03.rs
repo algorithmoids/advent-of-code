f193b4317c096a110165f36f1a5dc4489d34a98e
Dzmitry Talkach <chakmidlot@gmail.com>
2022-12-04 02:37:37 +0100

    2022 Day 3 part 2

--- year_2022/src/day_03.rs
+++ year_2022/src/day_03.rs
@@ -10,17 +10,55 @@ pub fn part_1() -> i32 {
 
     for items in read_input() {
         let pocket_size = items.len() / 2;
-        let pocket_1 = HashSet::<char>::from_iter(items.chars().take(pocket_size));
-        let pocket_2 = HashSet::<char>::from_iter(items.chars().rev().take(pocket_size));
-        let common = pocket_1.intersection(&pocket_2).last().expect("No common itmes");
+        let pockets = vec![
+            items.chars().take(pocket_size).collect(),
+            items.chars().rev().take(pocket_size).collect(),
+        ];
 
-        priority += if common > &'Z' {
-            (*common as i32) - 'a' as i32 + 1
-        }
-        else {
-            *common as i32 - 'A' as i32 + 27
-        }
+        let common = get_common_item(pockets);
+
+        priority += get_item_priority(&common)
     }
 
     priority
+}
+
+pub fn part_2() -> i32 {
+    let mut priority: i32 = 0;
+
+    let input = read_input();
+
+    for group in (0 .. input.len()).step_by(3) {
+        let pockets = vec![
+                input[group], input[group + 1], input[group + 2]
+            ]
+            .into_iter()
+            .map(|x| x.chars().collect())
+            .collect();
+
+        let common = get_common_item(pockets);
+
+        priority += get_item_priority(&common)
+    }
+
+    priority
+}
+
+fn get_common_item(groups: Vec<Vec<char>>) -> char {
+    groups.into_iter()
+        .map(|x| HashSet::<char>::from_iter(x))
+        .reduce(|a, b|
+            a.intersection(&b).into_iter().map(|x| *x).collect()
+        )
+        .expect("No common items")
+        .into_iter().collect::<Vec<_>>()[0]
+}
+
+fn get_item_priority(item: &char) -> i32 {
+    if item > &'Z' {
+        (*item as i32) - 'a' as i32 + 1
+    }
+    else {
+        *item as i32 - 'A' as i32 + 27
+    }
 }
--- year_2022/src/main.rs
+++ year_2022/src/main.rs
@@ -8,4 +8,5 @@ fn main() {
     println!("Day 2.1: {}", day_02::part_1());
     println!("Day 2.2: {}", day_02::part_2());
     println!("Day 3.1: {}", day_03::part_1());
+    println!("Day 3.2: {}", day_03::part_2());
 }
