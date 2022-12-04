e16e8881ca9f341808bf46ae29dd24a904848801
Dzmitry Talkach <chakmidlot@gmail.com>
2022-12-04 14:02:42 +0100

    2022 Day 4 part 1

---
+++ year_2022/src/day_04.rs
@@ -0,0 +1,26 @@
+use regex::Regex;
+
+fn read_input() -> Vec<Vec<i32>> {
+    let re = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+
+    let input = include_str!("input/day_04.txt");
+    input.split("\n")
+        .map(|x| re.captures(x).expect("Failed to parse input"))
+        .map(|cpatures| cpatures.iter()
+            .skip(1)
+            .map(|x| x.unwrap().as_str().parse::<i32>().expect("Failed parsing input number"))
+            .collect::<Vec<_>>())
+        .collect()
+}
+
+pub fn part_1() -> i32 {
+    let mut including = 0;
+
+    for input in read_input() {
+        if (input[0] >= input[2] && input[1] <= input[3]) || (input[2] >= input[0] && input[3] <= input[1]) {
+            including += 1;
+        }
+    }
+
+    including
+}
--- year_2022/src/main.rs
+++ year_2022/src/main.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 fn main() {
     println!("Day 1.1: {}", day_01::part_1());
@@ -9,4 +10,5 @@ fn main() {
     println!("Day 2.2: {}", day_02::part_2());
     println!("Day 3.1: {}", day_03::part_1());
     println!("Day 3.2: {}", day_03::part_2());
+    println!("Day 4.1: {}", day_04::part_1());
 }
