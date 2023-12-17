1eaa547c8598fec2e1d2200b6a9c23e87c3ebf65
Dzmitry Talkach <chakmidlot@gmail.com>
2023-12-18 00:00:21 +0100

    2023 Day 1 part 2

--- year_2023/src/day_01.rs
+++ year_2023/src/day_01.rs
@@ -1,22 +1,71 @@
+use std::collections::HashMap;
+use regex::Regex;
+
+
 fn read_input() -> Vec<&'static str> {
     let input = include_str!("input/day_01.txt");
     input.split("\n").collect()
 }
 
 pub(crate) fn part_1() -> u32 {
-    let mut sum = 0;
+    let mapping = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+
+    get_sum(mapping)
+}
+
+pub(crate) fn part_2() -> u32 {
+    let mapping = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+
+    get_sum(mapping)
+}
+
+fn get_sum(mapping: HashMap<&str, u8>) -> u32 {
+    let mut sum: u32 = 0;
+
+    let numbers_re = mapping.iter().map(|(k, _)| *k).collect::<Vec<_>>().join("|");
+    let first_re = Regex::new(&format!(".*?({}).*", numbers_re)).unwrap();
+    let last_re = Regex::new(&format!(".*({}).*", numbers_re)).unwrap();
 
     for row in read_input() {
+        let first = get_part(row, &first_re, &mapping);
+        let last = get_part(row, &last_re, &mapping);
 
-        let mut digits = row.chars().into_iter()
-            .filter(|x| x.is_digit(10));
-
-        let first = digits.next().unwrap().to_digit(10).unwrap();
-        let last = digits.last().map(|x| x.to_digit(10).unwrap()).unwrap_or(first);
-
-        sum += first * 10 + last
-
+        sum += (first * 10 + last) as u32;
     }
-
     sum
+}
+
+fn get_part(text: &str, pattern: &Regex, map: &HashMap<&str, u8>) -> u8 {
+    let captures = pattern.captures(text).unwrap();
+    let number = captures.get(1).unwrap().as_str();
+    return map[number]
 }
--- year_2023/src/main.rs
+++ year_2023/src/main.rs
@@ -4,6 +4,7 @@ use std::time::Instant;
 
 fn main() {
     run_measure("Day 1.1", &day_01::part_1);
+    run_measure("Day 1.2", &day_01::part_2);
 }
 
 fn run_measure<T: std::fmt::Display>(name: &str, function: &dyn Fn() -> T) {
